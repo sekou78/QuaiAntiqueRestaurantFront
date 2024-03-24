fd5c677bd239f7b6e5d3287880b3486157b1261c
Sekou CISSE <s.cisse78@gmail.com>
2024-03-24 12:21:12 +0100

    creation de la page Account + rangement du dossier p

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -4,8 +4,9 @@ import Route from "./Route.js";
 export const allRoutes = [
   new Route("/", "Accueil", "/pages/home.html"),
   new Route("/galerie", "La galerie", "/pages/galerie.html"),
-  new Route("/signin", "Connexion", "/pages/signin.html"),
-  new Route("/signup", "Inscription", "/pages/signup.html"),
+  new Route("/signin", "Connexion", "/pages/auth/signin.html"),
+  new Route("/signup", "Inscription", "/pages/auth/signup.html"),
+  new Route("/account", "Mon compte", "/pages/auth/account.html"),
 ];
 
 //Le titre s'affiche comme ceci : Route.titre - websitename
