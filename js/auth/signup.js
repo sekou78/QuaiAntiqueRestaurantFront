a811b1d44565b1ef879629b47b8a2c6732be4eb5
Sekou CISSE <s.cisse78@gmail.com>
2024-03-24 18:28:51 +0100

    verification de la confirmation du mot de passe

--- js/auth/signup.js
+++ js/auth/signup.js
@@ -20,8 +20,9 @@ function validateForm() {
   const prenomOk = validateRequired(inputPreNom);
   const mailOk = validateMail(inputMail);
   const passwordOk = validatePassword(inputPassword);
+  const passwordConfirmOk = validateConfirmationPassword(inputPassword, inputValidationPassword);
 
-  if (nomOk && prenomOk && mailOk && passwordOk) {
+  if (nomOk && prenomOk && mailOk && passwordOk && passwordConfirmOk) {
     btnValidation.disabled = false;
   } else {
     btnValidation.disabled = true;
@@ -71,6 +72,19 @@ function validatePassword(input) {
   else {
     input.classList.remove("is-valid");
     input.classList.add("is-invalid");
+    return false;
+  }
+}
+//Function permettant de verifier le champ confirmez votre mot de passe du formulaire
+function validateConfirmationPassword(inputPwd, inputConfirmPwd) {
+  if (inputPwd.value == inputConfirmPwd.value) {
+    inputConfirmPwd.classList.add("is-valid");
+    inputConfirmPwd.classList.remove("is-invalid");
+    return true;
+  }
+  else {
+    inputConfirmPwd.classList.add("is-invalid");
+    inputConfirmPwd.classList.remove("is-valid");
     return false;
   }
 }
