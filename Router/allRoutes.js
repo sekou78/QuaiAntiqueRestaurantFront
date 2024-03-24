162e17c3a64c026a8e2dd4f22984e2ca3feff864
Sekou CISSE <s.cisse78@gmail.com>
2024-03-24 16:42:02 +0100

    edition de la reservation

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -9,6 +9,7 @@ export const allRoutes = [
   new Route("/account", "Mon compte", "/pages/auth/account.html"),
   new Route("/editPassword", "Changement de mot de passe", "/pages/auth/editPassword.html"),
   new Route("/allResa", "Vos reservations", "/pages/reservations/allResa.html"),
+  new Route("/reserver", "Reserver", "/pages/reservations/reserver.html"),
 ];
 
 //Le titre s'affiche comme ceci : Route.titre - websitename
