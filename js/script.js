c22002fa2a085ca62459b6fdace6e583de12122e
Sekou CISSE <s.cisse78@gmail.com>
2024-03-26 13:17:22 +0100

    gestion de la deconnexion

--- js/script.js
+++ js/script.js
@@ -1,4 +1,7 @@
 const tokenCookieName = "accesstoken";
+const signoutBtn = document.getElementById("signout-btn");
+
+signoutBtn.addEventListener("click", signout);
 
 function setToken(token) {
   setCookie(tokenCookieName, token, 7);
@@ -44,4 +47,10 @@ if (isConnected()) {
   alert("Je suis connecté");
 } else {
   alert("Je ne suis pas connecté");
+}
+
+//fonction de deconnexion
+function signout() {
+  eraseCookie(tokenCookieName);
+  window.location.reload();
 }
