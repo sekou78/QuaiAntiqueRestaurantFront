8a0e7969418cc7b4c6d42e64cf4008b865d0372b
sekou78 <s.cisse78@gmail.com>
2024-03-30 12:13:33 +0100

    Utilisation d’un linter(sonarlint)

--- js/script.js
+++ js/script.js
@@ -14,21 +14,21 @@ function getToken() {
 }
 
 function setCookie(name, value, days) {
-  var expires = "";
+  let expires = "";
   if (days) {
-    var date = new Date();
+    let date = new Date();
     date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
     expires = "; expires=" + date.toUTCString();
   }
   document.cookie = name + "=" + (value || "") + expires + "; path=/";
 }
 function getCookie(name) {
-  var nameEQ = name + "=";
-  var ca = document.cookie.split(';');
-  for (var i = 0; i < ca.length; i++) {
-    var c = ca[i];
-    while (c.charAt(0) == ' ') c = c.substring(1, c.length);
-    if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
+  let nameEQ = name + "=";
+  let ca = document.cookie.split(';');
+  for (const element of ca) {
+    let c = element;
+    while (c.startsWith(' ')) c = c.substring(1, c.length);
+    if (c.startsWith(nameEQ)) return c.substring(nameEQ.length, c.length);
   }
   return null;
 }
@@ -44,13 +44,13 @@ function isConnected() {
     return true;
   }
 }
-
+/*
 // if (isConnected()) {
 //   alert("Je suis connecté");
 // } else {
 //   alert("Je ne suis pas connecté");
 // }
-
+*/
 //fonction de deconnexion
 function signout() {
   eraseCookie(tokenCookieName);
